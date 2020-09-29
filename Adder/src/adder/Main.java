8c3ae14730694253ceb47a6ea7dca0a7019fafc3
Norma Montejano <normajm11@yahoo.com>
2020-09-28 21:04:12 -0500

    Step 11 completed

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -13,5 +13,31 @@ public class Main {
 
     private static int addArguments(String[] args) {
         return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
-    }
-}
+	
+	#Modify the method addArguments to add any number of positive 
+	#integers instead of just two. 
+	
+	int sum = 0;
+	
+	for(int i= 0; i < args.length; i++){
+		sum += args[i];
+
+
+
+        # subtracts all the positive integers passed as parameters if the
+        #first argument is - (a dash.)
+
+		if (args[0] == '-') {
+			for(int i = 1; i < args.length; i++){
+				sum -= args[i];
+			}	
+		}
+		else {
+			for(int i= 0; i < args.length; i++){
+                	sum += args[i];
+			}
+		}
+
+		return sum;
+    	}
+    }	
